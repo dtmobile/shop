00eda9061671120f506ae2301f9dcbbedeb7c509
wuzhijun <wuzhijun@iwaimai.baidu.com>
2017-07-05 20:56:01 +0800

    还款过程添加还款方式选项

--- data/config.php
+++ data/config.php
@@ -30,7 +30,7 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2017-07-05 10:17:10');
+define('API_TIME', '2017-07-05 20:46:29');
 
 define('DEBUG_MODE', 3);
 //0 //禁用调试模式
